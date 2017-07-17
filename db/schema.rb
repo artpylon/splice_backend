2e1714318d175c75b084c26d4e5bb44e238b3f35
artpylon <matthewgray88@gmail.com>
2017-07-17 12:17:40 -0400

    Db migrate card resource Ran bin/rails db:migrate to migrate changes for card resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170717160656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "shape"
+    t.string   "color"
+    t.integer  "number"
+    t.string   "shading"
+    t.string   "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
