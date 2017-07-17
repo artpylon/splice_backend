18f7fed6e567f0e7e26b252928ccb8922aad78fe
artpylon <matthewgray88@gmail.com>
2017-07-17 12:38:55 -0400

    Migrate AddUserToGames update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717160656) do
+ActiveRecord::Schema.define(version: 20170717163548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20170717160656) do
     t.index ["user_id"], name: "index_examples_on_user_id", using: :btree
   end
 
+  create_table "games", force: :cascade do |t|
+    t.integer  "sets_found"
+    t.boolean  "over"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "token",           null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema.define(version: 20170717160656) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "games", "users"
 end
