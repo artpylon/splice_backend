1e3c697ea977db9e7d1729cbc63e2b8710593873
artpylon <matthewgray88@gmail.com>
2017-07-20 11:53:25 -0400

    Current user games controller Added current user to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < OpenReadController
-  before_action :set_game, only: [:update, :destroy]
+  before_action :set_game, only: [:show, :update, :destroy]
 
   # GET /games
   def index
@@ -41,7 +41,8 @@ class GamesController < OpenReadController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = current_user.games.find(params[:id])
+      # @game = current_user.games.find(params[:id])
+      @game = Game.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
