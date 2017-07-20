7d9d903795a356cb75e68569521001d4a8491fc7
artpylon <matthewgray88@gmail.com>
2017-07-20 15:06:19 -0400

    Update seeds.rb function, now working locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
-csv_text = File.read(Rails.root.join('db', 'cards.csv'))
-csv_text = csv_text.squish
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
-  Card.create!(Hash[row])
+csv_text = File.read(Rails.root.join('public', 'cards_csv.csv'))
+# csv_text = csv_text.squish
+csv = CSV.parse(csv_text, :headers => true)
 
+csv.each do |row|
+  Card.create!(row.to_hash)
 end
+
+# csv.each do |row|
+#   Card.create({
+#   shape: row[0],
+#   color: row[1],
+#   number: row[2],
+#   shading: row[3],
+#   img: row[4]
+#   })
+# end
+puts csv_text
+puts csv
+#
+# puts "There are now #{Card.count} rows in the cards table"
+
+# csv.each do |row|
+#   Card.create(Hash[row])
+#
+# end
